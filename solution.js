497a8d6367fe085913049c314a3a57be50b12f50
BabciaDokuczliwa <oblaczynska@gmail.com>
2022-06-30 15:24:40 +0200

    ex 2

--- solution.js
+++ solution.js
@@ -1,1 +1,27 @@
 // write your code in this file
+const allTicketCost = 6450;
+const ticketCost = 15;
+
+console.log(`Q1.`, allTicketCost / ticketCost,`tickets are sold`) ;
+
+const sylviaIncome = 500;
+const dayPerWeek = 7;
+const allDaysInYear = 365;
+console.log( `Q2.Sylvia makes every year`,  sylviaIncome / dayPerWeek * allDaysInYear);
+
+
+console.log(`Q3. 17 percentage from 30 is`, 0.17 * 30);
+console.log(`Q4.The perimeter of a square is `, 4.75 * 4, `cm`);
+
+console.log(`Q5. The perimeter of a triangle is ${5 + 6 + 7} cm`);
+console.log(`Q6. The area of a square is ${5 * 5} cm²`);
+console.log(`Q7. The area of an right-angled triangle  ${3 * 4 / 2} cm²`);
+console.log(`Q8. The volume of a cube ${9 * 9 * 9} cm³`);
+
+console.log(`Q9a. The three bills including tips are ${22.35 / 100 * 10}€ Q9b.${26.67 / 100 * 15}€ Q9a.${35.92 / 100 * 20}€`);
+
+console.log(`10. Noemy's average hours worked per day ${(8 + 6 + 5 + 9 + 8 + 2 + 1 + 8.5 + 7 + 4) / 10}h`);
+ 
+console.log(`Q11: Score in the sixth test: ${85 * 6 - (75 + 70 + 85 + 90 + 100)}`);
+
+console.log(`Q12: James needs a minimum of ${80 * 9 - 78 * 8}% to get an 80% average`);
